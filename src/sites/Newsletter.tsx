413cd0ac4513b10f878fcd7c88a5436bd4fc5fdf
BergFilip <bergusf2006@gmail.com>
2025-03-10 08:55:46 +0100

    Add Newsletter part1

--- src/sites/Newsletter.tsx
+++ src/sites/Newsletter.tsx
@@ -1,11 +1,48 @@
-import '../styles/sites/Newsletter.scss'
+import Footer from "../components/Footer.tsx";
+import Header from "../components/Header.tsx";
+import "../styles/sites/newsletter.scss"
+import React, { useState } from "react";
 
-function Newsletter() {
+function App() {
     return(
         <div className="container">
-
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
         </div>
     )
 }
 
-export default Newsletter
+const Main = () => {
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setShowAlert(true);
+    };
+
+    return (
+        <main>
+            <h1>Zapisz się do Newslettera</h1>
+            <p>nie pozwól, aby ominęły cię promocje i nowe atrakcje</p>
+                <form>
+                    <input type="email" placeholder={"jan.kowalski@wp.pl"}/>
+                    <input type="submit" value="Zapisz się" onClick={handleClick} className="alert-button"/>
+                    {showAlert && (
+                        <div className="alert-box">
+                            <div className="alert-header">
+                                <span>Zgłoszono pomyślnie</span>
+                                <button className="alert-close" onClick={() => setShowAlert(false)}>×</button>
+                            </div>
+                            <div className="alert-body">
+                                Dziękujemy za zapisanie się do naszego Newslettera. Sprawdź wiadomość e-mail.
+                            </div>
+                            <button className="alert-button-ok" onClick={() => setShowAlert(false)}>Ok</button>
+                        </div>
+                    )}
+                </form>
+        </main>
+    )
+};
+
+export default App
