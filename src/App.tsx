b4ce3729876751d7534aa8b76f2266e1d6eae9e4
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-03-11 16:07:49 +0100

    add pp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import "./index.scss";
 import './App.scss'
 import Footer from "./components/Footer.tsx";
 import Header from "./components/Header.tsx";
+import {BrowserRouter as Router,Routes, Route, } from "react-router-dom";
+import PrivacyPolicies from "./sites/Privacy_policy.tsx";
+
 
 function App() {
-  return(
-    <div className="container">
-        <Header></Header>
-        <Footer></Footer>
-    </div>
-  )
+    return(
+        <Router>
+            <div className="container">
+                <Header></Header>
+                <main className="main-container">
+                    <Routes>
+                        <Route path="/privacypolicies" element={<PrivacyPolicies/>}/>
+                    </Routes>
+                </main>
+                <Footer></Footer>
+            </div>
+        </Router>
+    )
 }
 
 export default App
---
+++ src/sites/Newsletter.tsx
@@ -0,0 +1,15 @@
+import "../styles/sites/Privacy_policy.scss"
+
+function PrivacyPolicies() {
+    return(
+        <main className="privacy">
+            <h3>Polityka prywatności</h3>
+            <div>
+                <p>Data wejścia w życie: 01.02.2025</p>
+                <p>Ostatnia aktualizacja: 01.02.2025</p>
+            </div>
+        </main>
+    );
+}
+
+export default PrivacyPolicies;
