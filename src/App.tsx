a55bf6bb617caacdae8e34fa48aff9e37d190cda
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-03-12 08:14:46 +0100

    user app 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import "./index.scss";
 import './App.scss'
 import Footer from "./components/Footer.tsx";
 import Header from "./components/Header.tsx";
+import {BrowserRouter as Router,Routes, Route, } from "react-router-dom";
+import User from "./sites/User.tsx";
+
 
 function App() {
-  return(
-    <div className="container">
-        <Header></Header>
-        <Footer></Footer>
-    </div>
-  )
+    return(
+        <Router>
+            <div className="container">
+                <Header></Header>
+                <main className="main-container">
+                    <Routes>
+                        <Route path="/user" element={<User/>}/>
+                    </Routes>
+                </main>
+                <Footer></Footer>
+            </div>
+        </Router>
+    )
 }
 
 export default App
---
+++ src/sites/User.tsx
@@ -0,0 +1,12 @@
+import "../styles/sites/User.scss"
+
+function User() {
+    return(
+        <main className="user">
+            <div className="container">
+            </div>
+        </main>
+    );
+}
+
+export default User;
