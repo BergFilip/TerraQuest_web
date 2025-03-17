0d84ef64bbc0102d6c485037043d2f19beda1560
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-03-17 14:37:49 +0100

    add section7

--- src/components/places_section_6.tsx
+++ src/components/places_section_6.tsx
@@ -4,18 +4,14 @@ type PlacesSection = {
     link: string;
     text1: string;
     text2: string;
-    text3: string;
-    text4: string;
-    text5: string;
 };
 
-const Places_5 = ({ link, text1, text2, text3, text4, text5 }: PlacesSection) => {
-    return <div className="Places_5">
+const Places_6 = ({ link, text1, text2}: PlacesSection) => {
+    return <div className="Places_6">
         <img src={link}/>
         <h4>{text1}</h4>
         <h4 className="descr_h4">{text2}</h4>
-        <p><span className="span1">{text3}</span> <span className="span2">{text4}</span> <span className="span1">{text5}</span> </p>
     </div>
 };
-export default Places_5;
+export default Places_6;
 
--- src/sites/Home.tsx
+++ src/sites/Home.tsx
@@ -6,6 +6,7 @@ import Places_2 from "../components/places_section_2.tsx";
 import Places_3 from "../components/places_section_3.tsx";
 import Places_4 from "../components/places_section_4.tsx";
 import Places_5 from "../components/places_section_5.tsx";
+import Places_6 from "../components/places_section_6.tsx";
 
 
 function Home() {
@@ -60,23 +61,45 @@ function Home() {
                     <HSection text1={"Szukaj według rodzaju obiektu"}
                               text2={""}></HSection>
                     <div className="places_section_3">
-                        <Places_3 link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"} text2={"Hotele"}></Places_3>
-                        <Places_3 link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"} text2={"Ośrodki wypoczynkowe"}></Places_3>
-                        <Places_3 link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"} text2={"Apartamenty"}></Places_3>
-                        <Places_3 link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"} text2={"Wille"}></Places_3>
+                        <Places_3
+                            link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"}
+                            text2={"Hotele"}></Places_3>
+                        <Places_3
+                            link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"}
+                            text2={"Ośrodki wypoczynkowe"}></Places_3>
+                        <Places_3
+                            link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"}
+                            text2={"Apartamenty"}></Places_3>
+                        <Places_3
+                            link={"https://u.profitroom.pl/2020-andersiahotel-pl/thumb/2560x1440/uploads/Zdjecia/IBBAndersiaHotel_1.jpg"}
+                            text2={"Wille"}></Places_3>
                     </div>
                 </div>
                 <div className="section5">
                     <HSection text1={"Polska - zobacz ! "}
                               text2={"Te popularne miejsca mają wiele do zaoferowania"}></HSection>
                     <div className="places_section_4">
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Gdańsk"} text2={"1 000 obiektów"}></Places_4>
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Warszawa"} text2={"1 000 obiektów"}></Places_4>
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Poznań"} text2={"1 000 obiektów"}></Places_4>
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Kołobrzeg"} text2={"1 000 obiektów"}></Places_4>
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Kraków"} text2={"1 000 obiektów"}></Places_4>
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Karpacz"} text2={"1 000 obiektów"}></Places_4>
-                        <Places_4 link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"} text1={"Wrocław"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Gdańsk"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Warszawa"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Poznań"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Kołobrzeg"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Kraków"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Karpacz"} text2={"1 000 obiektów"}></Places_4>
+                        <Places_4
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Wrocław"} text2={"1 000 obiektów"}></Places_4>
                     </div>
                 </div>
                 <div className="section6">
@@ -86,17 +109,41 @@ function Home() {
                     <div className="places_section_5">
                         <Places_5
                             link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
-                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"} text5={"785zł"}></Places_5>
+                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"}
+                            text5={"785zł"}></Places_5>
 
                         <Places_5
                             link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
-                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"} text5={"785zł"}></Places_5>
+                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"}
+                            text5={"785zł"}></Places_5>
                         <Places_5
                             link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
-                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"} text5={"785zł"}></Places_5>
+                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"}
+                            text5={"785zł"}></Places_5>
                         <Places_5
                             link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
-                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"} text5={"785zł"}></Places_5>
+                            text1={"Hotel Super Star"} text2={"Polska, Warszawa"} text3={"2 noce"} text4={"1432zł"}
+                            text5={"785zł"}></Places_5>
+                    </div>
+                </div>
+                <div className="section7">
+                    <HSection text1={"Poszukaj inspiracji na kolejną podróż"}
+                              text2={"Z nami nie ominą Cię żadne atrakcje"}></HSection>
+
+                    <div className="places_section_6">
+                        <div className="inspiration_big">
+                            <img src="https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"/>
+                            <h4>{"6 niepowtarzalnych domów wakacyjnych w Austrii"}</h4>
+                            <h4 className="descr_h4">{"Wakacje z wiekszą ekipą? Trafiłeś idealnie!"}</h4>
+                        </div>
+                        <Places_6
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Nudzisz się w jednym miejscu? To może podróż przez Hiszpanie?"} text2={"6 dni, 4 hotele i brak nudy. Idealne dla wszystkich poszukiwaczy przygód."}></Places_6>
+                        <Places_6
+                            link={"https://cf.bstatic.com/static/img/theme-index/bg_luxury/869918c9da63b2c5685fce05965700da5b0e6617.jpg"}
+                            text1={"Najlepsze domki na drzewie na świecie"} text2={"Zwykłe hotele są dla ciebie już nudne? To może noc w łonie natury."}></Places_6>
+
+
                     </div>
                 </div>
             </div>
