4fbfd7b2ab0f4198ba08ebc3c4858850f7109d66
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-04-08 21:18:04 +0200

    edit jakiÃs fajny

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ function App() {
                         <Route path="/contact" element={<Contact/>}/>
                         <Route path="/login" element={<Login/>}/>
                         <Route path="/register" element={<Register/>}/>
+                        <Route path="/product" element={<Product/>}/>
                     </Routes>
                 </main>
                 <Footer></Footer>
