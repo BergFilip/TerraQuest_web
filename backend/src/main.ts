891e730a82a7376ad255107ce5d38db73813140c
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-04-22 19:06:45 +0200

    edit port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ import reviewsRouter from './routes/reviews';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cookieParser());
